363ae3b92b1043b364c51bb0c189724286d7fd45
mouaadelkhadari <mouaad.elkhadari@gmail.com>
2025-03-27 16:48:26 +0000

    Adjusted HackerRoom's precise placement

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,8 +3,47 @@ import {PerspectiveCamera} from "@react-three/drei";
 import HackerRoom from "../components/HackerRoom.jsx";
 import {Suspense} from "react";
 import CanvasLoader from "../components/CanvasLoader.jsx";
+import {Leva, useControls} from "leva";
 
 const Hero = () => {
+    const x = useControls('HeckerRoom', {
+        positionX: {
+                value: 2.5,
+                min: -10,
+                max: 10
+            },
+        positionY: {
+            value: 2.5,
+            min: -10,
+            max: 10
+        },
+        positionZ: {
+            value: 2.5,
+            min: -10,
+            max: 10
+        },
+        rotationX: {
+            value: 0,
+            min: -10,
+            max: 10
+        },
+        rotationY: {
+            value: 0,
+            min: -10,
+            max: 10
+        },
+        rotationZ: {
+            value: 0,
+            min: -10,
+            max: 10
+        },
+        scale: {
+            value: 1,
+            min: 0.1,
+            max: 10
+        }
+
+    })
     return (
         <section className="min-h-screen w-full flex flex-col relative">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -15,16 +54,22 @@ const Hero = () => {
             </div>
 
             <div className="w-full h-full absolute inset-0">
+                <Leva />
                 <Canvas className="w-full h-full">
-                    <Suspense fallback={<CanvasLoader/>}>
-                        <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-                        <HackerRoom
-                            scale={[0.05]}
-                            position={[0, 0, 0]}
-                            rotation={[0, -Math.PI / 2, 0]}
-                        />
+                    <Suspense fallback={<CanvasLoader />}>
+
+                    <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
+                    <HackerRoom  // scale={0.07}
+                                 // position={[0, 0, 0]}
+                                 // rotation={[0, 280, 0]}
+                        position={[2, -10, 2]}
+                        rotation={[0, -Math.PI , 0]}
+                        scale={0.1}
+                    />
+
+
                         <ambientLight intensity={1} />
-                       <directionalLight position={[10, 10, 10]} intensity={0.5} />
+                        <directionalLight position={[0, 0, 0]} intensity={0.5} />
                     </Suspense>
                 </Canvas>
             </div>
